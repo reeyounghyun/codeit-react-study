0298782b9987274e88a136293b3d3278c7b1f312
reeyounghyun <naem77716@gmailcom>
2024-07-20 20:34:59 +0900

    git add Study data

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Button from './Button';
+import Dice from './Dice';
+
+function App() {
+  return (
+    <div>
+      <Button>던지기</Button>
+      <Button>처음부터</Button>
+      <Dice color="red" num={4} />
+    </div>
+  );
+}
+
+export default App;
